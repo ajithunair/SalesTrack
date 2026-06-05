b1755ee74ebe70dfd4e716193207721b5573de64
Nair,Ajith <Ajith.Nair@Cerner.com>
2026-06-06 01:04:25 +0530

    Identity & Postgres Db

--- SalesTrack/SalesTrack.Api/Program.cs
+++ SalesTrack/SalesTrack.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SalesTrack.Api.Areas.Identity.Data;
 namespace SalesTrack.Api;
 
 public class Program
@@ -5,7 +8,13 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
 
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(connectionString);
+        });
+        
         // Add services to the container.
 
         builder.Services.AddControllers();
